918c748f9bb4fa76eeeac8840e2ccffc5665367f
muhammadjewel <msuratov@gmail.com>
2023-10-06 23:27:22 +0500

    Update input on number key click. Clear input on Esc

--- js/main.js
+++ js/main.js
@@ -28,7 +28,75 @@ function init () {
   }
 
   const elCurrentTheme = document.documentElement.dataset.theme;
-  document.querySelector(`.theme-switcher__radio[value="${elCurrentTheme}"]`).setAttribute('checked', true);
+  document.querySelector(`.theme-switcher__radio[value='${elCurrentTheme}']`).setAttribute('checked', true);
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+
+// CALCULATOR
+const CALCULATOR = {
+  currentValue: '',
+  firstNumber: 0,
+  secondNumber: 0,
+  result: 0,
+  // TODO refactor to use key.code, instead of .key
+  keys: {
+    numbers: ['.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+    operators: ['+', '-', '*', '/'],
+    commands: ['Escape', 'Enter', 'Tab', 'Shift', 'Control', 'Alt', 'Delete', 'Backspace', 'F5']
+  },
+  input: '.calculator__input',
+  keyboard: '.calculator__keyboard',
+  // To emulate key press
+  pressKey: function (key) {
+    console.log(key);
+  },
+  clearInput: function (elInput) {
+    elInput.value = '';
+    CALCULATOR.currentValue = '';
+  },
+  updateInput: function (elInput) {
+    elInput.value = parseFloat(CALCULATOR.currentValue, 10).toLocaleString({
+      minimumIntegerDigits: 0
+    });
+  }
+};
+
+
+const elCalcKeyboard = document.querySelector(CALCULATOR.keyboard);
+const elCalcInput = document.querySelector(CALCULATOR.input);
+
+
+document.addEventListener('keydown', function (evt) {
+  let isNotAllowedKey = !CALCULATOR.keys.numbers.includes(evt.key) && !CALCULATOR.keys.operators.includes(evt.key) && !CALCULATOR.keys.commands.includes(evt.key);
+
+  if (isNotAllowedKey) {
+    evt.preventDefault();
+  }
+
+  if (evt.key === 'Escape') {
+    CALCULATOR.clearInput(elCalcInput);
+  }
+});
+
+
+
+if (elCalcKeyboard) {
+  elCalcKeyboard.addEventListener('click', function (evt) {
+    let isNotANumberKey = !evt.target.classList.contains('key--number');
+    let isDecimalKey = evt.target.dataset.key === '.';
+
+    if (isNotANumberKey) {
+      return;
+    }
+
+    if (isDecimalKey && CALCULATOR.currentValue.includes('.')) {
+      return;
+    }
+
+    CALCULATOR.currentValue += evt.target.dataset.key;
+    console.log(CALCULATOR.currentValue);
+    CALCULATOR.updateInput(elCalcInput);
+  });
+}
