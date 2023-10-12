c91d6cdc352df7d94d0a96414d66cab511ec1efd
muhammadjewel <msuratov@gmail.com>
2023-10-12 22:54:29 +0500

    Play sound on theme change. Bug: after using different methods (hotkey, input change) theme update not working

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const CALCULATOR = {
+  isSoundOn: true,
   themes: {
     '1': 'dark',
     '2': 'light',
@@ -11,6 +12,7 @@ function init () {
   // **THEME SWITCHER
   const elRoot = document.documentElement;
   const elThemeSwitcherControlWrapper = document.querySelector('.theme-switcher__control-wrapper');
+  const elSwitchAudio = document.querySelector('#switch-audio');
 
 
   function switchTheme () {
@@ -19,6 +21,10 @@ function init () {
 
     elRoot.setAttribute('data-theme', dataTheme);
     localStorage.setItem('theme', dataTheme);
+
+    if (CALCULATOR.isSoundOn) {
+      playAudio(elSwitchAudio);
+    }
   }
 
   function resetThemeInputs () {
@@ -32,6 +38,11 @@ function init () {
   function updateThemeInput (theme) {
     const elCurrentTheme = theme || elRoot.dataset.theme;
     document.querySelector(`.theme-switcher__radio[value='${elCurrentTheme}']`).setAttribute('checked', true);
+  }
+
+  function playAudio (el) {
+    el.currentTime = 0;
+    el.play();
   }
 
   function handleThemeHotkeyKeyUp (themeIndex) {
@@ -61,3 +72,6 @@ function init () {
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+
+// TODO: Fix theme changing after hotkey/input change (and vice versa) cases
